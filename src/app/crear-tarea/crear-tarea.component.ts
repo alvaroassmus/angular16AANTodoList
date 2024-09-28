df800b13936ec466ad9044b1b0bc4ba11da411ad
Alvaro Assmus Nassar <alvaroassmus@gmail.com>
2024-09-27 20:24:29 -0500

    UPDATE->se finaliza la logica para agregar tarea con personas y habilidades dentro de cada persona a la variable tareaForm con sus respectivas validaciones

--- src/app/crear-tarea/crear-tarea.component.ts
+++ src/app/crear-tarea/crear-tarea.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormBuilder,
@@ -6,15 +7,13 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-crear-tarea',
   standalone: true,
+  selector: 'app-crear-tarea',
   templateUrl: './crear-tarea.component.html',
   styleUrls: ['./crear-tarea.component.css'],
-  imports: [ReactiveFormsModule, CommonModule, RouterModule],
+  imports: [ReactiveFormsModule, CommonModule],
 })
 export class CrearTareaComponent {
   tareaForm: FormGroup;
@@ -31,39 +30,44 @@ export class CrearTareaComponent {
     return this.tareaForm.get('personas') as FormArray;
   }
 
-  getHabilidades(personaIndex: number): FormArray {
-    return this.personas.at(personaIndex).get('habilidades') as FormArray;
-  }
-
   agregarPersona() {
-    const personaForm = this.fb.group({
-      nombre: ['', Validators.required],
-      edad: ['', [Validators.required, Validators.min(19)]],
-      habilidades: this.fb.array(['']),
+    const personaGroup = this.fb.group({
+      nombreCompleto: ['', Validators.required],
+      edad: [null, [Validators.required, Validators.min(18)]],
+      habilidades: this.fb.array([]),
     });
-    this.personas.push(personaForm);
+
+    this.personas.push(personaGroup);
   }
 
   eliminarPersona(index: number) {
     this.personas.removeAt(index);
   }
 
-  agregarHabilidad(index: number) {
-    const habilidades = this.personas.at(index).get('habilidades') as FormArray;
-    habilidades.push(this.fb.control('', Validators.required));
+  getHabilidades(personaIndex: number): FormArray {
+    return this.personas.at(personaIndex).get('habilidades') as FormArray;
   }
 
-  eliminarHabilidad(index: number, habilidadIndex: number) {
-    const habilidades = this.personas.at(index).get('habilidades') as FormArray;
+  agregarHabilidad(personaIndex: number) {
+    const habilidades = this.getHabilidades(personaIndex);
+    habilidades.push(
+      this.fb.group({
+        habilidad_value: ['', Validators.required],
+      })
+    );
+  }
+
+  eliminarHabilidad(personaIndex: number, habilidadIndex: number) {
+    const habilidades = this.personas
+      .at(personaIndex)
+      .get('habilidades') as FormArray;
     habilidades.removeAt(habilidadIndex);
   }
 
   onSubmit() {
+    console.log(this.tareaForm);
     if (this.tareaForm.valid) {
       console.log(this.tareaForm.value);
-      // Aquí puedes manejar la lógica para guardar la tarea
-    } else {
-      console.log('Formulario no válido');
     }
   }
 }
